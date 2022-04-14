0c3bf21f0821386077f8f84a8aac9132f87f229d
Tyranzx <70720366+Lewysan@users.noreply.github.com>
2022-04-14 08:06:45 -0700

    Create StellarSource.java

---
+++ src/main/java/me/tyranzx/mountingmobs/StellarSource.java
@@ -0,0 +1,10 @@
+package me.tyranzx.mountingmobs;
+
+import org.bukkit.ChatColor;
+
+public class StellarSource {
+
+    public static String c(String msg){
+        return ChatColor.translateAlternateColorCodes('&', msg);
+    }
+}
